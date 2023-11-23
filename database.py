f24ecc41c071d5487b97ba42cdd04516a276648b
Taam Paramee <taamtera@gmail.com>
2023-11-24 05:31:32 +0700

    init

--- database.py
+++ database.py
@@ -1,21 +1,168 @@
-# try wrapping the code below that reads a persons.csv file in a class and make it more general such that it can read in any csv file
-
-import csv, os
+import csv, os, copy
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
+class DB:
+    def __init__(self):
+        self.database = []
+        
+    def load(self, csv_file_name):
+        table_name = csv_file_name.split('.')[0]
+        table = []
+        with open(os.path.join(__location__, csv_file_name)) as f:
+            rows = csv.DictReader(f)
+            for r in rows:
+                table.append(dict(r))
+        self.upsert(Table(table_name, table))
+        return self
+    
+    def write(self, csv_file_name):
+        table = self.select(csv_file_name.split('.')[0])
+        if table.table.__len__() > 0:
+            # with open(os.path.join(__location__, csv_file_name), 'w', newline='') as f:
+            with open(os.path.join(__location__, csv_file_name),'w', newline='') as f:
+                writer = csv.DictWriter(f, fieldnames=table.table[0].keys())
+                writer.writeheader()
+                for row in table.table:
+                    writer.writerow(row)
+        return self
 
-persons = []
-with open(os.path.join(__location__, 'persons.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        persons.append(dict(r))
-print(persons)
+    def upsert(self, table):
+        if self.select(table.table_name) is not None:
+            self.drop(table.table_name)
+        self.database.append(table)
+        return self
 
-# add in code for a Database class
+    def join(self , table1_name, table2_name, common_key):
+        table1 = self.select(table1_name)
+        table2 = self.select(table2_name)
+        if table1 is not None and table2 is not None:
+            self.upsert(table1.join(table2, common_key))
+        return self
+    
+    def drop(self, table_name):
+        table = self.select(table_name)
+        if table is not None:
+            self.database.remove(table)
+        return self
 
-# add in code for a Table class
+    def select(self, table_name, where=None, columns=None):
+        for table in self.database:
+            if table.table_name == table_name:
+                if where is not None:
+                    table = table.filter(lambda item: all(item[key] == where[key] for key in where))
+                if table is not None and columns is not None:
+                    table = table.select(columns)
+                return table
+        return None
 
-# modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
+    def __str__(self) -> str:
+        tbnames = []
+        for table in self.database:
+            tbnames.append(table.table_name)
+        return str(tbnames)
+    
+    def print(self):
+        print(self)
+        return self
+    
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
+    
+    def join(self, other_table, common_key):
+        joined_table = Table(self.table_name + '_' + other_table.table_name, [])
+        for item1 in self.table:
+            for item2 in other_table.table:
+                if item1[common_key] == item2[common_key]:
+                    dict1 = copy.deepcopy(item1)
+                    dict2 = copy.deepcopy(item2)
+                    dict1.update(dict2)
+                    joined_table.table.append(dict1)
+        return joined_table
+    
+    def filter(self, condition):
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            if condition(item1):
+                filtered_table.table.append(item1)
+        if len(filtered_table.table) == 0:
+            return None
+        return filtered_table
 
-# modify the code in the Table class so that it supports the update operation where an entry's value associated with a key can be updated
+    def __is_float(self, element):
+        if element is None: 
+            return False
+        try:
+            float(element)
+            return True
+        except ValueError:
+            return False
+
+    def aggregate(self, function, aggregation_key):
+        temps = []
+        for item1 in self.table:
+            if self.__is_float(item1[aggregation_key]):
+                temps.append(float(item1[aggregation_key]))
+            else:
+                temps.append(item1[aggregation_key])
+        return function(temps)
+    
+    def select(self, attributes_list):
+        temps = Temps()
+        for item1 in self.table:
+            dict_temp = {}
+            for key in item1:
+                if key in attributes_list:
+                    dict_temp[key] = item1[key]
+            temps.append(dict_temp)
+        return temps
+    
+    def upsert(self, data, where=None):
+        if where is None:
+            if self.table.__len__() == 1:
+                self.table[0].update(data)
+            else:
+                self.table.append(data)
+        else:
+            for item in self.table:
+                if all(item[key] == where[key] for key in where):
+                    item.update(data)
+        return self
+    
+    def drop(self, where=None):
+        if where is None:
+            self.table = []
+        else:
+            for item in self.table:
+                if all(item[key] == where[key] for key in where):
+                    self.table.remove(item)
+        return self
+
+    def __str__(self):
+        return self.table_name + ':' + str(self.table)
+    
+    def __len__(self):
+        return self.table.__len__()
+    
+    def print(self):
+        for item in self.table:
+            print(item)
+        return self
+
+class Temps:
+    def __init__(self):
+        self.temps = []
+        
+    def append(self, item):
+        self.temps.append(item)
+        return self
+    
+    def print(self):
+        for item in self.temps:
+            print(item)
+        return self
+    
+    def __str__(self):
+        return str(self.temps)
--- project_manage.py
+++ project_manage.py
@@ -1,61 +1,288 @@
-# import database module
+import random ,sys, os
+from database import DB
 
-# define a funcion called initializing
+db = DB()
+user = None
+
 
 def initializing():
-    pass
+    db.load('persons.csv')\
+        .load('login.csv')\
+        .load('projects.csv')\
+        .load('requests.csv')\
+            
+def admin():
+    while True:
+        os.system('cls' if os.name == 'nt' else "printf '\033c'")
+        print('Welcome admin. select a table to edit type "back" to exit admin mode.')
+        db.print()
+        unip = uinput()
+        if unip == 'back':
+            return None
+        if db.select(unip) is not None:
+            tb = unip
+            while True:
+                os.system('cls' if os.name == 'nt' else "printf '\033c'")
+                db.select(tb).print()
+                print('Enter the "ID" of the entry you would like to edit or')
+                print('"new" add new entry')
+                print('"delete" delete entry')
+                print('"back" to go back')
+                unip = uinput()
+                if unip == 'back':
+                    break
+                elif unip == 'new':
+                    db.select(tb).table.append({'ID': rnd_id()})
+                    for key in db.select(tb).table[0].keys():
+                        if key != 'ID':
+                            db.select(tb).table[0][key] = uinput('Enter ' + key + ': ')
+                    db.write(tb + '.csv')
+                elif unip == 'delete':
+                    db.select(tb).drop(where={'ID': uinput('Enter ID of entry to delete: ')})
+                    db.write(tb + '.csv')
+                elif db.select(tb, where={'ID': unip}) is not None:
+                    entry = db.select(tb, where={'ID': unip})
+                    print('leave blank to keep current value')
+                    for key in entry.table[0].keys():
+                        if key != 'ID':
+                            unip = uinput('Enter ' + key + ': ')
+                            if unip != '':
+                                entry.table[0][key] = unip
+                    db.write(tb + '.csv')
+    
 
-# here are things to do in this function:
+def get_project():
+    unip = ''
+    new= True
+    if user['type'] == 'admin':
+        string = """Select a project to work on by "name" or "ID" or enter "admin" for admin mode."""
+        print(line(len(string)))
+        print(string)
+        print(line(len(string)))
+        db.select('projects').select(['ID', 'name', 'project_data', 'stage']).print()
+        print(line(len(string)))
+        unip = uinput()
+        if unip == 'admin':
+            admin()
+            return None
+    else:
+        string = """Select a project to work on by "name" or "ID"."""
+        print(line(len(string)))
+        print(string)
+        print(line(len(string)))
+        if db.select('projects',where={'head': user['ID']}) is not None:
+            new = False
+            print('Your projects: ')
+            db.select('projects',where={'head': user['ID']}).select(['ID', 'name', 'project_data', 'stage']).print()
+            print(line(len(string)))
+        if db.select('projects',where={'member1': user['ID']}) or db.select('projects',where={'member2': user['ID']}) is not None:
+            new = False
+            print('Projects you are a member of: ')
+            if db.select('projects',where={'member1': user['ID']}) is not None:
+                db.select('projects',where={'member1': user['ID']}).select(['ID', 'name', 'project_data', 'stage']).print()
+            if db.select('projects',where={'member2': user['ID']}) is not None:
+                db.select('projects',where={'member2': user['ID']}).select(['ID', 'name', 'project_data', 'stage']).print()
+            print(line(len(string)))
+        if db.select('projects',where={'advisor': user['ID']}) is not None:
+            new = False
+            print('Your Student\'s projects: ')
+            db.select('projects',where={'advisor': user['ID']}).select(['ID', 'name', 'project_data', 'stage']).print()
+            print(line(len(string)))
+        if new:
+            unip = uinput('You have no projects. type "New" to make a new project: ')
+        else:
+            unip = uinput('or "New" to make a new project: ')
 
-    # create an object to read all csv files that will serve as a persistent state for this program
+    if unip == '':
+        return None
+    unip = unip.lower()
+    project_id = db.select('projects', where={'ID': unip}) or db.select('projects', where={'name': unip})
+    if project_id is not None:
+        edit_project(project_id)
+    elif unip == 'new':
+        create_project()
 
-    # create all the corresponding tables for those csv files
+def edit_project(project_id):
+    while True:
+        db.load('projects.csv')
+        project = db.select('projects', where={'ID': project_id.table[0]['ID']})
+        os.system('cls' if os.name == 'nt' else "printf '\033c'")
+        print('Project data: ')
+        print(line(len('Project data: ')))
+        project.select(['ID', 'name', 'project_data', 'stage']).print()
+        head = db.select('persons', where={'ID': project.table[0]['head']})
+        print('project head: ' + head.table[0]['fist'] + ' ' + head.table[0]['last'])
+        member1 = db.select('persons', where={'ID': project.table[0]['member1']})
+        if member1 is not None:
+            print('project member1: ' + member1.table[0]['fist'] + ' ' + member1.table[0]['last'])
+            member2 = db.select('persons', where={'ID': project.table[0]['member2']})
+            if member2 is not None:
+                print('project member2: ' + member2.table[0]['fist'] + ' ' + member2.table[0]['last'])
+        advisor = db.select('persons', where={'ID': project.table[0]['advisor']})
+        if advisor is not None:
+            print("Your Student's projects: " + advisor.table[0]['fist'] + ' ' + advisor.table[0]['last'])
+        string = 'what would you like to do with this project: '
+        print(line(len(string)))
+        if project.table[0]['head'] == user['ID'] or user['type'] == 'admin':
+            print('"1" Edit project data')
+            print('"2" Invite member')
+            print('"3" Request advisor')
+            print('"4" submit project')
+            print('"5" Delete project')
+        elif project.table[0]['advisor'] == user['ID']:
+            print('"5" abandon your studetns')
+        else:
+            print('"1" Edit project data')
+            print('"5" leave project')
+        print('"back" to go back')
+        print(line(len(string)))
+        unip = uinput(string)
+        if project.table[0]['head'] != user['ID'] and unip in ['2' '3', '4'] and user['type'] != 'admin':
+            unip = 0
+        os.system('cls' if os.name == 'nt' else "printf '\033c'")
+        match unip:
+            case '1':
+                project.upsert({'project_data': uinput('Enter new project data: ')})
+                db.write('projects.csv')
+            case '2':
+                print('Enter the ID of the person you would like to invite.')
+                db.select('persons',where={'type': 'student'}).select(['ID', 'fist', 'last']).print()
+                unip = uinput()
+                if db.select('persons', where={'ID': unip}) is not None:
+                    if project.table[0]['member1'] == '':
+                        send_request(project, unip, 'member1')
+                    elif project.table[0]['member2'] == '':
+                        send_request(project, unip, 'member2')
+                    else:
+                        uinput('This project is full.')
+                else:
+                    uinput('Invalid ID.')
+            case '3':
+                print('Enter the ID of the person you would like to invite.')
+                db.select('persons',where={'type': 'faculty'}).select(['ID', 'fist', 'last']).print()
+                unip = uinput()
+                if db.select('persons', where={'ID': unip}) is not None:
+                    send_request(project, unip, 'advisor')
+                else:
+                    uinput('Invalid ID.')
+            case '4':
+                if project.table[0]['advisor'] is '':
+                    uinput('You need to request an advisor first.')
+                else:
+                    send_request(project.table[0]['ID'], project.table[0]['advisor'], 'submit')
+                    project.upsert({'stage': 'submitted'})
+                    uinput('Project submitted.')
+            case '5':
+                if project.table[0]['head'] != user['ID']:
+                    project.upsert({'advisor': None}, where={'advisor': user['ID']})
+                    project.upsert({'member1': None}, where={'member1': user['ID']})
+                    project.upsert({'member2': None}, where={'member2': user['ID']})
+                    db.write('projects.csv')
+                    return None
+                project.select(['ID', 'name', 'project_data', 'stage']).print()
+                string = uinput('Are you sure you want to delete this project? enter the id of the project to confirm:')
+                if string == project.table[0]['ID']:
+                    db.select('projects').drop(where={'ID': project_id.table[0]['ID']})
+                    return None
+            case 'back':
+                return None
 
-    # see the guide how many tables are needed
+def create_project():
+    # ID,name,head,member1,member2,advisor,project_data
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")
+    data = {'ID': rnd_id(), 'name': uinput('Enter project name: '), 'head': user['ID'], 'member1': None, 'member2': None, 'advisor': None, 'project_data': uinput('Enter project data: '),'stage': 'draft'}
+    db.select('projects').upsert(data).write('projects.csv')
 
-    # add all these tables to the database
+def get_request():
+    requests = db.select('requests', where={'ID': user['ID']})
+    if requests is None:
+        print('You have no requests')
+    else:
+        print('You have ' + str(len(requests)) + ' requests.')
+        for request in requests.table:
+            print('{Project: "' + request['project_name'] + '" type: "' + request['type'] +'"}')
+            print('Enter "accept" to accept the request or "decline" to decline it.')
+            unip = uinput()
+            if unip == 'accept':
+                if request['type'] == 'member1':
+                    db.select('projects').upsert({'member1': user['ID']}, where={'ID': request['project']})
+                elif request['type'] == 'member2':
+                    db.select('projects').upsert({'member2': user['ID']}, where={'ID': request['project']})
+                elif request['type'] == 'advisor':
+                    db.select('projects').upsert({'advisor': user['ID']}, where={'ID': request['project']})
+                elif request['type'] == 'submit':
+                    db.select('projects').upsert({'stage': 'accepted'}, where={'ID': request['project']})
+                db.select('requests').drop(where={'ID': user['ID'], 'project': request['project']})
+                print('Request accepted.\n')
+                db.write('projects.csv').write('requests.csv')
+            elif unip == 'decline':
+                if request['type'] == 'submit':
+                    db.select('projects').upsert({'stage': 'draft'}, where={'ID': request['project']})
+                db.select('requests').drop(where={'ID': user['ID'], 'project': request['project']})
+                print('Request declined.\n')
+                db.write('projects.csv').write('requests.csv')
+    return None
 
+def send_request(project,user_id,type):
+    db.select('requests').upsert({'ID': user_id, 'project': project.table[0]['ID'], 'type': type, 'project_name': project.table[0]['name']})
+    uinput('Request sent.')
+    db.write('requests.csv')
 
-# define a funcion called login
+def rnd_id():
+    return str(random.randint(1000000, 9999999))
+
+def line(leng):
+    return ''.join(['-' for _ in range(leng)])
+
+def uinput(string = ''):
+    unip = input(string).lower()
+    if unip == 'exit':
+        exit()
+    return unip
 
 def login():
-    pass
+    # ask the user to enter the username and password 3 times
+    for _ in range(3):
+        # username = uinput("Enter username: ")
+        # password = uinput("Enter password: ",clear=true)
+        username = 'Taam.P'
+        password = '1234'
+        os.system('cls' if os.name == 'nt' else "printf '\033c'")
+        # check if the user id exists and the password matches
+        login_id = db.select('login', where={'username': username, 'password': password})
+        if login_id:
+            return login_id.table[0]['ID']
+        print('Login failed. Please try again.')
+    print('You have exceeded the maximum number of tries. Please try again later.')
+    return None
 
-# here are things to do in this function:
-   # add code that performs a login task
-        # ask a user for a username and password
-        # returns [ID, role] if valid, otherwise returning None
-
-# define a function called exit
 def exit():
-    pass
+    # write out all the tables that have been modified to the corresponding CSV files
+    db.write('persons.csv')\
+        .write('login.csv')\
+        .write('projects.csv')\
+        .write('requests.csv')
+    sys.exit()
 
-# here are things to do in this function:
-   # write out all the tables that have been modified to the corresponding csv files
-   # By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
-   
-   # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
+def main():
+    # make calls to the initializing and login functions defined above
+    initializing()
+    user_id = login()
+    if user_id is None:
+        uinput()
+        return
+    global user
+    user = db.select('persons', where={'ID': user_id}).table[0]
+    while True:
+        initializing()
+        os.system('cls' if os.name == 'nt' else "printf '\033c'")
+        string = 'Welcome '+ user['type'] + " " + user['fist'] + ' ' + user['last'] + '. type "exit" to exit.'
+        print( '\n' + line(len(string)))
+        print(string)
+        print(line(len(string)))
+        get_request()
+        get_project()
+
+main()
 
 
-# make calls to the initializing and login functions defined above
-
-initializing()
-val = login()
-
-# based on the return value for login, activate the code that performs activities according to the role defined for that person_id
-
-# if val[1] = 'admin':
-    # see and do admin related activities
-# elif val[1] = 'student':
-    # see and do student related activities
-# elif val[1] = 'member':
-    # see and do member related activities
-# elif val[1] = 'lead':
-    # see and do lead related activities
-# elif val[1] = 'faculty':
-    # see and do faculty related activities
-# elif val[1] = 'advisor':
-    # see and do advisor related activities
-
-# once everyhthing is done, make a call to the exit function
-exit()
